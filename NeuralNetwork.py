dd8ed2dd33afe2f4a010ad62092475cca7f12c44
ParthSareen <psareen@uwaterloo.ca>
2022-10-12 16:06:34 -0500

    WIP

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,27 +1,25 @@
 import numpy as np
+from Layer import *
 
 
 class NeuralNetwork:
-    def __init__(self, learning_rate=0.01):
-        self.layers = []
+    def __init__(self, layers=None, learning_rate=0.01):
+        self.layers = layers
         self.learning_rate = learning_rate
-        self.__setup__()
-
-    def __setup__(self):
-        # call methods with Layer class to set up the network initially
-        pass
 
     def train(self, input_mat: np.ndarray, output_mat: np.ndarray, epochs: int):
 
         pass
 
     def single_pass(self, input_vec: np.ndarray, output_vec: np.ndarray):
-        pass
+        output_vec_pred = self.predict(input_vec)
+        loss_vec = self.loss(output_vec, output_vec_pred)
+        return loss_vec
 
     def predict(self, input_vec: np.array) -> np.ndarray:
         pass
 
-    def loss(self, output_vec: np.array, output_vec_pred: np.array):
+    def loss(self, output_vec: np.array, output_vec_pred: np.array) -> np.ndarray:
         pass
 
     def display_grad(self):
---
+++ TDD.py
@@ -0,0 +1,10 @@
+import NeuralNetwork
+from NeuralNetwork import *
+
+
+def main():
+    NN = NeuralNetwork.NeuralNetwork()
+
+
+if __name__ == '__main__':
+    main()
