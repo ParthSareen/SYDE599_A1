4dfda8e283c64cd675f36d15a640397b956ff1af
Shyam <shyamu.hari@gmail.com>
2022-10-12 16:30:44 -0400

    added typing to parameters

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,19 +8,21 @@ class NeuralNetwork:
         self.__setup__()
 
     def __setup__(self):
+
         # call methods with Layer class to set up the network initially
         pass
 
-    def train(self, input_mat, output_mat, epochs):
+    def train(self, input_mat: np.ndarray, output_mat: np.ndarray, epochs: int):
+
         pass
 
-    def single_pass(self, input_vec, output_vec):
+    def single_pass(self, input_vec: np.ndarray, output_vec: np.ndarray):
         pass
 
-    def predict(self, input_vec):
+    def predict(self, input_vec: np.array) -> np.ndarray:
         pass
 
-    def loss(self, output_vec, output_vec_pred):
+    def loss(self, output_vec: np.array, output_vec_pred: np.array):
         pass
 
     def display_grad(self):
